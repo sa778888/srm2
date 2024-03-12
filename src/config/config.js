35db1c50973e517c2f084ac6d745e905642846aa
Aadarsh <aadarshjha6783@gmail.com>
2024-03-13 05:07:56 +0530

    Added env file deploying scripts

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,14 @@
-export default {
-    "TSTToken": "0x31213FF9ff4296B747028041eF354002aD785b85",
-    "SHMToken": "0x2d9FE6DFa994261c46e3a203ba6619726BB05e63",
-    "UniswapRouter": "0xECA7c18438c86F63Cc8a56F376Da97Fd77ccC365",
-    Slippage:2,//2%
-    contracts: {
-        GIT_CONTRACT: "0x17486C4e34b2b6c6690C835Ab7bbDA6e5722E239",
-        MAIN_CONTACT: "0xf665904F8656Cbef3eBF6aDEb1f2D10F3705b35D"
-    }
-}
+import { z } from "zod"
+
+const envSchema = z.object({
+    TSTToken: z.string().default("0x31213FF9ff4296B747028041eF354002aD785b85"),
+    SHMToken: z.string().optional().default("0x2d9FE6DFa994261c46e3a203ba6619726BB05e63"),
+    UniswapRouter: z.string().optional().default("0xECA7c18438c86F63Cc8a56F376Da97Fd77ccC365"),
+    Slippage: z.number().default(2),
+    GIT_CONTRACT: z.string().default("0x17486C4e34b2b6c6690C835Ab7bbDA6e5722E239"),
+    MAIN_CONTRACT: z.string(),
+    PRIVATE_KEY: z.string(),
+    SHARDEUM_RPC: z.string()
+});
+
+export const config = envSchema.parse(process.env);
