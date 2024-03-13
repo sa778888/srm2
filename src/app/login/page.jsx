3f1bc82fa2a89fa60be3a5f98f4ea4ac7afd66c5
Aadarsh <aadarshjha6783@gmail.com>
2024-03-13 10:56:28 +0530

    Basic metamask login interface

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,15 +4,31 @@ import { Button } from '@/components/ui/button'
 import { WalletContext } from '@/context/WalletContext'
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { toast } from 'sonner'
 const gra = "bg-gradient-to-r from-purple-400 to-yellow-400"
 const page = () => {
-    const { connected, signer } = useContext(WalletContext);
+    const { connected, signer, connectWallet,disconnectWallet } = useContext(WalletContext);
+    const [isLoading, setIsLoading] = useState(false);
     const handleMetamask = () => {
-        return;
-    }
+        if (!window.ethereum) {
+            toast.error('Metamask not found')
+            return;
+        }
+        setIsLoading(true);
+        try {
+            const result = connectWallet();
 
+        } catch (error) {
+            setIsLoading(false);
+        }
+
+    }
+    useEffect(() => {
+        if (connected) {
+            toast.success('Connected to Metamask');
+        }
+    }, [connected])
     return (
         <div className='bg-zinc-700'>
             <BackgroundGradientAnimation firstColor='FDA403' secondColor='430A5D' thirdColor='FF8E8F' fourthColor='124076' className='flex w-screen h-screen overflow-hidden items-center justify-center'>
@@ -21,14 +37,20 @@ const page = () => {
                     <div className="rounded-full p-6 bg-black/20">
                         <Image src='/svg/metamask.svg' width={100} height={100} />
                     </div>
-                    <Button variant={"accent"} className="text-white"
-                        onClick={handleMetamask}
+                    {
+                        connected ? (<Button variant={"accent"} className="text-white"
+                        onClick={disconnectWallet}
                     >
+                        Disconnect Wallet <ArrowRight className="w-4 h-4 ml-2" />
+                    </Button>): (<Button variant = { "accent" } className = "text-white"
+                        onClick = { handleMetamask }
+                        >
                         Get Started with Metamask <ArrowRight className="w-4 h-4 ml-2" />
-                    </Button>
-                </div>
-            </BackgroundGradientAnimation>
+                </Button>)
+                    }
         </div>
+            </BackgroundGradientAnimation >
+        </div >
     )
 }
 
